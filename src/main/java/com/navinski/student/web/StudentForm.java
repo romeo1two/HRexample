cea9ba925374e978ff4c386c5d3aff61f54055bf
romeo1two <romeo1two@gmail.com>
2018-01-24 10:14:43 +0300

    commit 50

--- src/main/java/com/navinski/student/web/StudentForm.java
+++ src/main/java/com/navinski/student/web/StudentForm.java
@@ -49,8 +49,8 @@ public class StudentForm {
 		this.educationYear = educationYear;
 	}
 	// comments for new line here today
-	// comment #49
-	// or some other comments tommorow
+	// comment #50
+	// or some other comments now
 	public int getGroupId() {
 		return groupId;
 	}
