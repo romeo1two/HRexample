bad896f57d4fabc901f763bbfa9dcff9ec6e6496
Mikalai Navinski <romeo1two@gmail.com>
2018-02-02 15:34:42 +0300

    commit 65-6

---
+++ src/main/java/Test.java
@@ -0,0 +1,93 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass<A> test1 = new TestClass<A>();
+        TestClass<A> test2 = new TestClass<A>();
+        TestClass<B> test3 = new TestClass<B>();
+        // CountObject - static property in class TestClass 
+        System.out.println("Test1: " + test1.CountObject); // 2
+        System.out.println("Test2: " + test2.CountObject); // 1
+        System.out.println("Test3: " + test3.CountObject);
+	
+	}
+}
+
+class TestClass<T> {
+	public static int CountObject =  0;
+	
+	public TestClass()
+	{
+	    CountObject++;
+	}
+}
+
+class A {
+	public void Display()
+	{
+		System.out.println("Test2");
+	}
+}
+
+class B extends A
+{
+	@Override
+	public void Display()
+	{
+		System.out.println("New value");
+	}
+}
+
+
+
+
+
+
+
+
+
+//namespace ConsoleApp2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            TestClass<A> test1 = new TestClass<A>();
+//            TestClass<A> test2 = new TestClass<A>();
+//            TestClass<B> test3 = new TestClass<B>();
+//            // CountObject - static property in class TestClass 
+//            Console.WriteLine(TestClass<A>.CountObject); // 2
+//            Console.WriteLine(TestClass<B>.CountObject); // 1
+//
+//            Console.ReadKey();
+//        }
+//    }
+
+//    class TestClass<T> 
+//        where T : A
+//    {
+//        public static int CountObject { get; set; }
+//
+//        public TestClass()
+//        {
+//            CountObject++;
+//        }
+//    }
+//
+//    public class A
+//    {
+//        public virtual void Display()
+//        {
+//            Console.WriteLine("From A");
+//        }
+//    }
+//
+//    public class B : A
+//    {
+//        public override void Display()
+//        {
+//            Console.WriteLine("From B");
+//        }
+//    }
+//}
