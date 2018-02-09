a7e3c9de8d61a28371a1f70654f564402d7a6ad8
Mikalai Navinski <romeo1two@gmail.com>
2018-02-09 11:11:23 +0300

    commit 70-3

--- src/main/java/com/navinski/student/entity/Students.java
+++ src/main/java/com/navinski/student/entity/Students.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-// updated in february
+// updated fresh
 @Entity
 @Table (name = "students")
 @NamedQueries({
